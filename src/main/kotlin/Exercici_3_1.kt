753362e5ffd7314bb6eb5c36bbf99ef3a90ba784
asieralbalate <alualb1634@ieselcaminas.org>
2023-10-13 02:34:33 +0200

    last

--- src/main/kotlin/Exercici_3_1.kt
+++ src/main/kotlin/Exercici_3_1.kt
@@ -2,16 +2,16 @@ import java.io.DataInputStream
 import java.io.FileInputStream
 
 fun main(args: Array<String>) {
-    val f = DataInputStream(FileInputStream("Rutes.dat"))
+    val f = DataInputStream(FileInputStream("Rutes.dat")) //asigna el archivo a f
 
-    while (f.available() > 0) {
-        println("Ruta: " + f.readUTF())
+    while (f.available() > 0) {  //bucle para leer en el fileInputStream
+        println("Ruta: " + f.readUTF())  //sabiendo cada tipo de archivo leemos
         println("Desnivell: " + f.readInt())
         println("Desnivell acumulat: " + f.readInt())
-        val numPoints: Int = f.readInt()
+        val numPoints: Int = f.readInt() //lee los puntos y hace un bucle
         println("Té: $numPoints punts")
         for (i in 1..numPoints){
-        println("Punt $i: " + f.readUTF() + " (" + f.readDouble() + "," + f.readDouble() + ")")
+        println("Punt $i: " + f.readUTF() + " (" + f.readDouble() + "," + f.readDouble() + ")") //lee los puntos
         }
         println("\n")
     }
